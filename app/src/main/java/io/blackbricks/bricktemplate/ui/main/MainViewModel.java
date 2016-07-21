a937d536c0d431da02617cd5975e425d3b81792e
Kryndach <kryndach.ios@gmail.com>
2016-07-21 12:13:07 +0600

    removed action bar

--- app/src/main/java/io/blackbricks/bricktemplate/ui/main/MainViewModel.java
+++ app/src/main/java/io/blackbricks/bricktemplate/ui/main/MainViewModel.java
@@ -1,9 +1,5 @@
 package io.blackbricks.bricktemplate.ui.main;
 
-import android.databinding.ObservableField;
-import android.view.View;
-
-
 import cz.kinst.jakub.viewmodelbinding.ViewModel;
 import io.blackbricks.bricktemplate.databinding.ActivityMainBinding;
 
