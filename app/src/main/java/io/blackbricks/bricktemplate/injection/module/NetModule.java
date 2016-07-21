d7e9947d178d53370b97d7528f35ebe3cddac0bd
Kryndach <kryndach.ios@gmail.com>
2016-07-21 16:18:14 +0600

    cleanup net module

--- app/src/main/java/io/blackbricks/bricktemplate/injection/module/NetModule.java
+++ app/src/main/java/io/blackbricks/bricktemplate/injection/module/NetModule.java
@@ -1,54 +1,19 @@
 package io.blackbricks.bricktemplate.injection.module;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import dagger.Module;
-import dagger.Provides;
-import io.blackbricks.bricktemplate.BuildConfig;
-import io.blackbricks.bricktemplate.injection.scope.PerApplication;
-import io.blackbricks.bricktemplate.service.api.ApiService;
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
-import rx.schedulers.Schedulers;
 
 /**
  * Created by yegorkryndach on 21/07/16.
  */
 @Module
 public class NetModule {
-    @Provides
-    @PerApplication
-    static Gson provideGson() {
-        return new GsonBuilder()
-                .create();
+    private String mBaseUrl;
+    private String mAppKey;
+
+    public NetModule(String baseUrl, String appKey) {
+        this.mBaseUrl = baseUrl;
+        this.mAppKey = appKey;
     }
 
-    @Provides
-    @PerApplication
-    static OkHttpClient provideOkHttpClient() {
-        return new OkHttpClient();
-    }
 
-    @Provides
-    @PerApplication
-    static ApiService provideApiService(Gson gson, OkHttpClient okHttpClient) {
-        OkHttpClient.Builder httpClientBuilder = okHttpClient.newBuilder();
-
-        if (BuildConfig.DEBUG) {
-            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-            httpClientBuilder.addInterceptor(loggingInterceptor);
-        }
-
-        return new Retrofit.Builder()
-                .baseUrl("https://sample.com/")
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.createWithScheduler(Schedulers.io()))
-                .callFactory(httpClientBuilder.build())
-                .build().create(ApiService.class);
-    }
 }
